ca200c527967c7bb7607de3f94f3789b6c76b74b
Erik Babu <eb1816@ic.ac.uk>
2017-12-27 13:41:53 +0000

    Re-wrote some of the functions to remove hard-coding thus making code more scalable. Allows as many dataset partitions as required now

--- world_gdp.py
+++ world_gdp.py
@@ -41,7 +41,7 @@ def get_json_formatted_data(filename):
       gdp_data = json.load(f)
   except IOError:
     print("Error. File " + filename + " does not exist.")
-    return None
+    quit()
   else:
     return gdp_data
 
@@ -55,64 +55,64 @@ def get_cc_gdps(gdp_data, year):
   for gdp_dict in gdp_data:
     if gdp_dict['Year'] == year:
       country_name = gdp_dict['Country Name']
-      gdp_val = get_correctly_formatted_gdp(gdp_dict['Value'])
+      gdp_val = gdp_in_billions(gdp_dict['Value'])
       code = get_country_code(country_name)
       if code:
         country_gdps[code] = gdp_val
   return country_gdps
 
-def get_correctly_formatted_gdp(gdp_val):
+def gdp_in_billions(gdp_val):
   """Returns GDP in $bn as an integer."""
   billion = 1000000000
   return int(float(gdp_val) / billion)
 
-def format_wm(colour_scheme, str_year, gdp_labels):
+def group_gdps_by_level(full_set, subset_ranges, levels):
+  """Partitions the data into the correct levels."""
+  #validate subsets and levels
+  num_ranges = len(subset_ranges)
+  num_levels = len(levels)
+  validate_subsets_and_levels(num_ranges, num_levels)
+
+  #map each range to one of the subset dictionaries
+  gdps = [{}, {}, {}, {}] #list of dictionaries stores gdp info for each range
+  gdp_partition = OrderedDict()
+  for index in range(0, num_ranges):
+    curr_range = subset_ranges[index]
+    gdp_partition[curr_range] = gdps[index]
+
+  #partition into appropriate subsets
+  for country, gdp in full_set:
+    for index in range(0, num_levels):
+      if gdp < levels[index]:
+        update_gdp_partition(gdp_partition, subset_ranges[index], country, gdp)
+    update_gdp_partition(gdp_partition, subset_ranges[index + 1], country, gdp)
+  return gdp_partition
+
+def validate_subsets_and_levels(num_ranges, num_levels):
+  """
+  Checks that there is 1 more range defined than the number of levels defined.
+  """
+  if num_ranges != num_levels + 1:
+    print("Number of subsets must be 1 greater than number of levels")
+    quit()
+
+def update_gdp_partition(partition, subset_range, country, gdp):
+  """Maps the correct gdp value to the subset range for each country."""
+  partition[subset_range][country] = gdp
+
+def format_wm(colour_scheme, str_year, gdp_partition):
   """
   Formats and saves the world map according to colour, year and datasets.
   """
   wm_style = RS(colour_scheme, base_style=LCS)
   wm = pygal.maps.world.World(style=wm_style)
   wm.title = 'World GDP in ' + str_year + ', by Country (labels in $bn)'
-  for label, dataset in gdp_labels.items():
+  for label, dataset in gdp_partition.items():
     wm.add(label, dataset)
 
   save_name = 'world_gdp_' + str_year + '.svg'
   wm.render_to_file(save_name)
 
-def group_gdps_by_level(full_set):
-  """Partitions the data into the correct levels."""
-  #numbers were originally divided by 1 billion
-  hundred_billion = 100
-  one_trillion    = 1000
-  ten_trillion    = 10000
-
-  #subset ranges
-  range_1 = '$0 - $100bn'
-  range_2 = '$100bn - $1tn'
-  range_3 = '$1tn - $10tn'
-  range_4 = '>$10tn'
-
-  gdps_1, gdps_2, gdps_3, gdps_4 = {}, {}, {}, {}
-
-  #create ordered dictionary to be returned
-  gdp_labels = OrderedDict()
-  gdp_labels[range_1] = gdps_1
-  gdp_labels[range_2] = gdps_2
-  gdp_labels[range_3] = gdps_3
-  gdp_labels[range_4] = gdps_4
-
-  #partition into appropriate subsets
-  for country, gdp in full_set:
-    if gdp < hundred_billion:
-      gdp_labels[range_1][country] = gdp
-    elif gdp < one_trillion:
-      gdp_labels[range_2][country] = gdp
-    elif gdp < ten_trillion:
-      gdp_labels[range_3][country] = gdp
-    else:
-      gdp_labels[range_4][country] = gdp
-
-  return gdp_labels
 
 #Capture and validate the year
 year_lower_bound = 1960
@@ -123,14 +123,13 @@ year = get_year(year_lower_bound, year_upper_bound)
 filename = 'gdp_json.json'
 gdp_data = get_json_formatted_data(filename)
 
-if not gdp_data:
-  quit()
-
 #build dictionary of gdp data
 country_gdps = get_cc_gdps(gdp_data, year)
 
-#group gdp data by level
-gdp_subsets = group_gdps_by_level(country_gdps.items())
+#group gdp data by ranges using levels (in billions)
+ranges = ['$0 - $100bn', '$100bn - $1tn', '$1tn - $10tn', '>$10tn']
+levels = [100, 1000, 10000]
+gdp_subsets = group_gdps_by_level(country_gdps.items(), ranges, levels)
 
 #format and save the world map
 colour_scheme = '#a02c09'
